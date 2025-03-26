400ec202d7f3305651152304fbc0df4c2a53497a
dilumhapugoda <dilumhapugoda@gmail.com>
2025-03-27 01:09:18 +0530

    first intent

--- app/src/main/java/com/example/fixcrew/LoginActivity.kt
+++ app/src/main/java/com/example/fixcrew/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fixcrew
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/fixcrew/MainActivity.kt
+++ app/src/main/java/com/example/fixcrew/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fixcrew
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +21,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
